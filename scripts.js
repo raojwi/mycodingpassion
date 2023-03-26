07285d7cc56257e1f06a268c38eed9836ad0ee6e
Florin Tanasescu <109978408+raojwi@users.noreply.github.com>
2023-03-27 00:39:38 +0300

    -

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// (function myfunction(){
+//     window.onload=function(){
+//         const cardu = document.getElementById("my_frame");
+//             cardu.addEventListener("mouseover",console.log("avem eveniment"),false)
+//             cardu.addEventListener("mouseleave",console.log("papa eveniment"),false)
+//       }
+
+// })();
