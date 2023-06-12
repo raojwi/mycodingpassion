15df389a25f6f2ad8a0431bb6d5d272bdee51be0
Florin Tanasescu <109978408+raojwi@users.noreply.github.com>
2023-06-12 23:25:01 +0300

    re
    re

--- scripts.js
+++
@@ -1,8 +0,0 @@
-// (function myfunction(){
-//     window.onload=function(){
-//         const cardu = document.getElementById("my_frame");
-//             cardu.addEventListener("mouseover",console.log("avem eveniment"),false)
-//             cardu.addEventListener("mouseleave",console.log("papa eveniment"),false)
-//       }
-
-// })();
